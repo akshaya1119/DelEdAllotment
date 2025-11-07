0b3b0e0b57cd871fc0939dcbd2b78bf32a4758ac
Madhavi Tripathi <madhavi@chandrakala.co.in>
2025-11-07 12:45:42 +0530

    Address added from admit card and warg added

---
+++ DelEdAllotment/Models/AdmitCard.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DelEdAllotment.Models
+{
+    public class AdmitCard
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Sr_No { get; set; }
+        public int Registration_No { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Pin { get; set; }
+        public string State { get; set; }
+    }
+}
